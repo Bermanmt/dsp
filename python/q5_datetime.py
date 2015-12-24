bbbdeeb04413cc648092694b355b92eaceb5ee69
Andrés Masís <bermanprogramming@gmail.com>
2015-12-24 16:37:41 -0600

    String Problems

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,41 @@
 # Hint:  use Google to find python function
+from datetime import datetime
 
 ####a) 
+date_format = '%m-%d-%Y'
 date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+date_stop = '07-28-2015'  
 
-####b)  
+start = datetime.strptime(date_start, date_format)
+stop =  datetime.strptime(date_stop, date_format)
+
+numberOfDays = stop-start
+print 'A)', numberOfDays.days
+
+####b) 
+date_format = '%d-%b-%Y'
 date_start = '12312013'  
 date_stop = '05282015'  
 
+date_start = datetime.fromtimestamp(float(date_start)).strftime('%d-%b-%Y')
+date_stop =  datetime.fromtimestamp(float(date_stop)).strftime('%d-%b-%Y')
+
+start = datetime.strptime(date_start, date_format)
+stop =  datetime.strptime(date_stop, date_format)
+
+numberOfDays = start-stop
+print 'B)',numberOfDays.days
+
 ####c)  
+date_format = '%d-%b-%Y'
 date_start = '15-Jan-1994'  
 date_stop = '14-Jul-2015'  
+
+start = datetime.strptime(date_start, date_format)
+stop =  datetime.strptime(date_stop, date_format)
+
+numberOfDays = stop-start
+print 'C)',numberOfDays.days
+
+
+
--- python/q6_strings.py
+++ python/q6_strings.py
@@ -18,7 +18,13 @@ def donuts(count):
     >>> donuts(99)
     'Number of donuts: many'
     """
-    raise NotImplementedError
+    if count < 0:
+        message = "The Number of Donuts has to be greater than cero"
+        return message
+    elif count >10:
+        return 'Number of donuts: many' 
+    else:
+        return 'Number of donuts: ' + str(count)
 
 
 def both_ends(s):
@@ -37,7 +43,11 @@ def both_ends(s):
     >>> both_ends('xyz')
     'xyyz'
     """
-    raise NotImplementedError
+    if len(s) > 2:
+        answer = s[:2]+s[-2]+s[-1]
+    else:
+        answer = ''
+    return answer
 
 
 def fix_start(s):
@@ -56,7 +66,8 @@ def fix_start(s):
     >>> fix_start('donut')
     'donut'
     """
-    raise NotImplementedError
+    answer = s[0] + s[1:].replace(s[0],'*')
+    return answer
 
 
 def mix_up(a, b):
@@ -74,7 +85,9 @@ def mix_up(a, b):
     >>> mix_up('pezzy', 'firm')
     'fizzy perm'
     """
-    raise NotImplementedError
+    first = b[:2]+a[2:]
+    second = a[:2]+b[2:]
+    return first + ' ' + second
 
 
 def verbing(s):
@@ -91,7 +104,13 @@ def verbing(s):
     >>> verbing('do')
     'do'
     """
-    raise NotImplementedError
+    if len(s)>=3:
+        if s[-3:] != 'ing':
+            return s+'ing'
+        else:
+            return s+'ly'
+    else:
+        return s
 
 
 def not_bad(s):
@@ -111,7 +130,12 @@ def not_bad(s):
     >>> not_bad("It's bad yet not")
     "It's bad yet not"
     """
-    raise NotImplementedError
+    not_index = s.find(' not')
+    bad_index = s.find(' bad')
+    if not_index < bad_index and not_index!=-1: 
+        return s[:not_index]+' good'+s[bad_index+4:]
+    else: 
+        return s
 
 
 def front_back(a, b):
@@ -130,4 +154,18 @@ def front_back(a, b):
     >>> front_back('Kitten', 'Donut')
     'KitDontenut'
     """
-    raise NotImplementedError
+    if len(a)%2 == 0:
+        a_front = a[:len(a)/2]
+        a_back = a[len(a)/2:]
+    else:
+        a_front = a[:(len(a)/2)+1]
+        a_back = a[(len(a)/2)+1:]
+
+    if len(b)%2 == 0:
+        b_front = b[:len(b)/2]
+        b_back = b[len(b)/2:]
+    else:
+        b_front = b[:(len(b)/2)+1]
+        b_back = b[(len(b)/2)+1:]
+
+    return a_front+b_front+a_back+b_back
